bd57cf1a12f05f9feef768bc452251502972f9ac
Sebastian Schepis <sschepis@gmail.com>
2013-12-30 12:55:12 -0800

    Incremented version to 0.6.5, mods in mingw makefile to support i686-mingw, uncommented insert in serialize.h to compile linux binaries.

--- src/serialize.h
+++ src/serialize.h
@@ -822,7 +822,6 @@ public:
     }
 
 
-/*
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
     {
         assert(last - first >= 0);
@@ -836,7 +835,6 @@ public:
             vch.insert(it, first, last);
     }
 
-*/
 
 #if !defined(_MSC_VER) || _MSC_VER >= 1300
     void insert(iterator it, const char* first, const char* last)
